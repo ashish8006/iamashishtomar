3632f02ec8cc35f45654647b90fdd172087ade7e
Ashish Tomar <ashish80pl@gmail.com>
2022-09-04 11:45:22 +0530

    Node js First Project

---
+++ controller/src/app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+let path = require('path');
+let port = process.env.PORT || 8000;
+const staticpath = path.join(__dirname,'../../views');
+app.use(express.static(staticpath));
+
+app.set('view engine', 'html');
+
+app.get('/',(req,res)=>{
+  res.render('index.html');
+});
+
+app.listen(8000,(req,res)=>{
+    console.log('Server Is Running Port Number 8000');
+});
